d07c78c9d33d2afe78a1a69ccc59374f7fc25b25
Phaenir <phaenir@gmail.com>
2016-06-07 19:48:11 +0200

    Database connection done

---
+++ B2B Web Form/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(B2B_Web_Form.Startup))]
+namespace B2B_Web_Form
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
